a156d8a9480e25638ab109a21431c458d8833ec5
Javier Ortiz <javier.ortiz989@gmail.com>
2021-12-09 10:08:28 -0800

    async

--- week8/thursday/demos/readline-demo/playground.js
+++ week8/thursday/demos/readline-demo/playground.js
@@ -1,16 +1,37 @@
 const wait = () => {
     return new Promise(resolve => {
         setTimeout(() => {
-            console.log("print this");
-            resolve(5)
+            // console.log("print this");
+            resolve()
         }, 2000);
     });
 
 }
 
-wait()
-    .then(num1 => {
-        console.log(num1)
-        return wait()
-    })
-    .then(num2 => console.log(num2));
+// wait()
+//     .then(num1 => {
+//         console.log(num1)
+//         return wait()
+//     })
+//     .then(num2 => console.log(num2));
+
+async function waiting() {
+
+    const num1 = await wait();
+    // console.log(num1);
+    const num2 = await wait();
+    // console.log(num2);
+    return num1 + num2;
+}
+
+// (async function() {
+//     const result = await waiting();
+//     console.log(result);
+// })();
+
+// waiting().then(result => console.log(result));
+
+(async function() {
+    await wait();
+    console.log("hello");
+})();
